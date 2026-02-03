7b6bcfaf8dce5f118438c29a65da51f24fce6664
aleksander-olesya <artemioskalganov@yandex.by>
2026-02-04 00:50:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function openCard() {
-    var card = document.querySelector('.card');
-    card.classList.toggle('open');
+// Проверяем Telegram Web App
+if (window.Telegram.WebApp) {
+    Telegram.WebApp.ready(); // Сигнал Telegram, что мини-апп готов
 }
+
+document.getElementById('rsvpButton').addEventListener('click', () => {
+    alert("Спасибо за ответ! Мы вас ждем 🎉");
+    // Здесь можно отправлять данные на сервер через fetch()
+});
